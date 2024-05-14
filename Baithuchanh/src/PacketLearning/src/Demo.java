674323477a56117bda7dfe86e6326022c1ccff63
DoQuangQuan <quangquan004@gmail.com>
2024-05-14 22:18:13 +0700

    upadte commit

---
+++ Baithuchanh/src/PacketLearning/src/Demo.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class Demo {
+    public static void main(String[] args) {
+        Person ps = new Person();
+        ps.nhapthongtin();
+        ps.Inthongtin();
+        ps.Inthongtin("Do Quang Quan", 19);
+       
+
+    }
+}
