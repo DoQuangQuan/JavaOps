0e04a46ce7130e73f334563229db793c734483d7
DoQuangQuan <quangquan004@gmail.com>
2024-05-21 22:51:57 +0700

    baithuchanh15/5

---
+++ Baithuchanh/src/Chuong_4/ngay_15_05_Page60.java
@@ -0,0 +1,33 @@
+package Chuong_4;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+public class ngay_15_05_Page60 {
+    public static void main(String[] args) {
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.add("Java");
+        linkedHashSet.add("C++");
+        linkedHashSet.add("Java");
+        linkedHashSet.add("PHP");
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("cac phan tu trong linkedHashSet");
+        System.out.println(linkedHashSet);
+        System.out.println("Nhap phan tu can xoa  ");
+        str =sc.nextLine();
+        if(linkedHashSet.contains(str)){
+            linkedHashSet.remove(str);
+            System.out.println("xoa thanh cong ");
+            System.out.println("cac phan tu con lai trong hashSetString");
+            System.out.println(linkedHashSet);
+
+        }else{
+            System.out.println("Xoa khong thanh cong ");
+            for (String s: linkedHashSet){
+                System.out.println(str);
+            }
+
+        }sc.close();
+    }
+}
+
